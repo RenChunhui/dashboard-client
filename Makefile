aa3151f9a24652ceb9e817f09da3255da6dc2401
renchunhui <346878847@qq.com>
2019-05-09 15:05:13 +0800

    form

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 # 生产环境
 build:
 	@yarn build
-	@find docs -name "*.js" -print0 | xargs -0 gzip -k
-	@find docs -name "*.css" -print0 | xargs -0 gzip -k
+	@find dist -name "*.js" -print0 | xargs -0 gzip -k
+	@find dist -name "*.css" -print0 | xargs -0 gzip -k
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  constructor(
-    public translate: TranslateService
-  ){
-    translate.addLangs(['en','zh']);
-    translate.setDefaultLang('en');
+export class AppComponent implements OnInit {
+    constructor(
+        public translate: TranslateService
+    ) {
+        translate.addLangs(['en', 'zh']);
+        translate.setDefaultLang('en');
 
-    const browserLang = translate.getBrowserLang();
-    translate.use(browserLang.match(/en|zh/) ? browserLang : 'zh');
-  }
+        const browserLang = translate.getBrowserLang();
+        translate.use(browserLang.match(/en|zh/) ? browserLang : 'zh');
+    }
+
+
+    ngOnInit() {
+        console.log('ngOnInit');
+    }
 }
